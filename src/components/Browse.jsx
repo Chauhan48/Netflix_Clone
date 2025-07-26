805c2ea836a98ce71256880cc2435a786e97e6b6
Anshit Chauhan <chauhananshit48@gmail.com>
2025-07-26 13:52:48 +0530

    feat: fetch TMDB API data to display on home screen

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS } from '../utils/constants'
 
 const Browse = () => {
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+      API_OPTIONS);
+    const json = await data.json();
+    console.log(json);
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, [])
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const LOGO = "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix
 export const PROFILE_PICTURE = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
 
 export const BACKGROUND_IMAGE = "https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTZiOTE2MGQzYTE2YWUxZmNjNDU0YmQ2MWIwMWIwZCIsIm5iZiI6MTc1MzUxNjA0MS45NDksInN1YiI6IjY4ODQ4ODA5MDFmN2QwMjQzODhhNjZjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3e5IyrjQZyfgsk0jw_RCB394v1uiFXiKXtnYkh5Bx_Q'
+    }
+};
